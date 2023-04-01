d66a5c7a706230d821db4cf62cce9c09f66e2e37
manuel <manuel.kl900@gmail.com>
2023-04-01 08:00:12 +0200

    update install.sh for steamos

--- linux/src/install.sh
+++ linux/src/install.sh
@@ -4,6 +4,8 @@ INSPATH="/usr/local/bin/dotconfig"
 # check if OpenWrt
 if grep -q OpenWrt /proc/version; then
     INSPATH="/usr/bin/"
+elif grep -q valve /proc/version; then
+    INSPATH="/usr/bin/"
 fi
 
 sudo ln -s $HOME/.dotconfig/linux/src/dotconfig.sh $INSPATH
